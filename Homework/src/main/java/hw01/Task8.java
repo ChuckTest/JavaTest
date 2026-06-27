37d5e668bf951d99312583a2438f78e774c43510
Chuck LU <chucklu2015@outlook.com>
2026-06-27 17:03:40 +0800

    adjust code

--- Homework/src/main/java/hw01/Task8.java
+++ Homework/src/main/java/hw01/Task8.java
@@ -3,7 +3,7 @@ package hw01;
 public class Task8 {
     public static void GetNarcissus() {
         for (int i = 100; i < 1000; i++) {
-            int a = i % 100 % 10;
+            int a = i % 10;
             int b = i / 10 % 10;
             int c = i / 100;
             int sum = (int) (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
