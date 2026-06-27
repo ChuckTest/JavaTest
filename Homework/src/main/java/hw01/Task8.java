502ad69bba639cf4fe0c15be47c4c8f1ca1843c9
Chuck LU <chucklu2015@outlook.com>
2026-06-27 17:03:16 +0800

    fix code

--- Homework/src/main/java/hw01/Task8.java
+++ Homework/src/main/java/hw01/Task8.java
@@ -3,16 +3,15 @@ package hw01;
 public class Task8 {
     public static void GetNarcissus() {
         for (int i = 100; i < 1000; i++) {
-            int a = i % 100;
-            int b = (i - a) % 10;
-            int c = i/100;
+            int a = i % 100 % 10;
+            int b = i / 10 % 10;
+            int c = i / 100;
             int sum = (int) (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
             if (sum == i) {
                 System.out.println(i);
-                System.out.println(String.format("i=%d,a=%d,b=%d,c=%d",i,a,b,c));
-            }
-            else{
-                //System.out.println(String.format("i=%d,a=%d,b=%d,c=%d",i,a,b,c));
+                System.out.println(String.format("i=%d,a=%d,b=%d,c=%d", i, a, b, c));
+            } else {
+                //System.out.println(String.format("i=%d,a=%d,b=%d,c=%d", i, a, b, c));
             }
         }
     }
