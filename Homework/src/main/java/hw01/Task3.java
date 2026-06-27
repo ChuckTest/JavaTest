765576fda32671a8546ff7e097d93575a5ce5638
Chuck LU <chucklu2015@outlook.com>
2026-06-27 15:44:44 +0800

    fix code and add Employee

--- Homework/src/main/java/hw01/Task3.java
+++ Homework/src/main/java/hw01/Task3.java
@@ -3,7 +3,7 @@ package hw01;
 public class Task3 {
     public class Person{
         public Person() {
-            this = new Person("张三", 20);
+            this ("张三", 20);
         }
 
         public Person(String tempName, int tempAge) {
@@ -15,6 +15,13 @@ public class Task3 {
         int age;
         void speak(){
             System.out.println(String.format("name=%s,age=%d",name,age));
+        }
+    }
+
+    public class Employee extends Person{
+        int salary;
+        void computeSalary(int hours,int rate) {
+            salary = hours * rate;
         }
     }
 }
